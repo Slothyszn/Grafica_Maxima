8c4686124f2ba3a5c54380df99725b9a9343e65a
Slothyszn <diasabraao96@gmail.com>
2025-11-04 19:50:30 -0300

    importando funções de inserir papel

--- front/main.js
+++ front/main.js
@@ -1,5 +1,8 @@
 import { ConsultarPapeis } from '../modulos/listar_papeis.js';
 import { desaparecer } from '../modulos/listar_papeis.js';
+import { InserirPapel } from '../modulos/inserir_papel.js'
 
 document.getElementById("btn-consulta").addEventListener("click", ConsultarPapeis);
 document.getElementById("btn-desapareca").addEventListener("click", desaparecer);
+
+document.getElementById("form-papel").addEventListener("submit", InserirPapel);
