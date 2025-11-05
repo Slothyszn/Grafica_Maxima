282b84c22db399587bad5daceab697d26339fcab
Slothyszn <diasabraao96@gmail.com>
2025-11-05 16:45:45 -0300

    Adicionando função de deletar papéis

--- front/main.js
+++ front/main.js
@@ -3,6 +3,8 @@ import { desaparecer } from '../modulos/listar_papeis.js';
 import { InserirPapel } from '../modulos/inserir_papel.js'
 import { carregarOpcoesPapeis } from '../modulos/listar_papeis.js';
 import { habilitarEdicao } from '../modulos/editar_papel.js'
+import { carregarPapeisParaExcluir, excluirPapel} from '../modulos/deletar_papel.js'
+
 
 document.getElementById("btn-consulta").addEventListener("click", ConsultarPapeis);
 document.getElementById("btn-desapareca").addEventListener("click", desaparecer);
@@ -12,3 +14,6 @@ document.getElementById("form-papel").addEventListener("submit", InserirPapel);
 window.addEventListener("DOMContentLoaded", carregarOpcoesPapeis);
 
 document.getElementById("tipoPapel").addEventListener("change", habilitarEdicao);
+
+document.getElementById("btnExcluir").addEventListener("click", excluirPapel);
+window.onload = carregarPapeisParaExcluir;
