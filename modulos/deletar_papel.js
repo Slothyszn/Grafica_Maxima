b7ee2c558b46e94f2198cc37958ded21bdd301b3
Slothyszn <diasabraao96@gmail.com>
2025-11-05 16:46:45 -0300

    Criando função de deletar papéis

---
+++ modulos/deletar_papel.js
@@ -0,0 +1,28 @@
+export async function carregarPapeisParaExcluir() {
+  const resposta = await fetch("http://localhost:3000/api/papeis");
+  const dados = await resposta.json();
+
+  const datalist = document.getElementById("listaPapeisExcluir");
+  datalist.innerHTML = "";
+  dados.Papel.forEach(p => {
+    const option = document.createElement("option");
+    option.value = p.nome;
+    datalist.appendChild(option);
+  });
+}
+
+export async function excluirPapel() {
+  const nome = document.getElementById("papelExcluir").value;
+  if (!nome) return alert("Selecione o papel que deseja excluir");
+
+  const confirmacao = confirm(`Tem certeza que deseja excluir o papel "${nome}"?`);
+  if (!confirmacao) return;
+
+  const resposta = await fetch(`http://localhost:3000/api/papeis/nome/${encodeURIComponent(nome)}`, {
+    method: "DELETE"
+  });
+
+  const data = await resposta.json();
+  alert(data.mensagem);
+  carregarPapeisParaExcluir(); // atualiza lista
+}
